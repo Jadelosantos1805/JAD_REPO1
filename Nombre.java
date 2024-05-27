f09ca34e3e7dcafe41fabffcc0355efb294d8228
Jadelosantos1805 <169069274+Jadelosantos1805@users.noreply.github.com>
2024-05-27 16:51:59 +0200

    Add files via upload

---
+++ Nombre.java
@@ -0,0 +1,13 @@
+package github;
+
+public class Nombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String nombre = "Jose antonio";
+		System.out.println("Hola "+nombre+"!!!!");
+		
+	}
+
+}
